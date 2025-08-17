08fd0c9f12f55b7a59098488a90ba7552610038e
mondaychen <monday.chen@gmail.com>
2025-08-17 17:00:34 -0400

    remove unused content in popup & fix url

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -4,38 +4,11 @@ import { PROJECT_URL_OBJECT, useStorage, withErrorBoundary, withSuspense } from 
 import { exampleThemeStorage } from '@extension/storage';
 import { cn, ErrorDisplay, LoadingSpinner, ToggleButton } from '@extension/ui';
 
-const notificationOptions = {
-  type: 'basic',
-  iconUrl: chrome.runtime.getURL('icon-34.png'),
-  title: 'Injecting content script error',
-  message: 'You cannot inject script here!',
-} as const;
-
 const Popup = () => {
   const { isLight } = useStorage(exampleThemeStorage);
   const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
 
   const goGithubSite = () => chrome.tabs.create(PROJECT_URL_OBJECT);
-
-  const injectContentScript = async () => {
-    const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    if (tab.url!.startsWith('about:') || tab.url!.startsWith('chrome:')) {
-      chrome.notifications.create('inject-error', notificationOptions);
-    }
-
-    await chrome.scripting
-      .executeScript({
-        target: { tabId: tab.id! },
-        files: ['/content-runtime/example.iife.js', '/content-runtime/all.iife.js'],
-      })
-      .catch(err => {
-        // Handling errors related to other paths
-        if (err.message.includes('Cannot access a chrome:// URL')) {
-          chrome.notifications.create('inject-error', notificationOptions);
-        }
-      });
-  };
 
   const openOptionsPage = () => {
     chrome.runtime.openOptionsPage();
@@ -56,14 +29,6 @@ const Popup = () => {
             )}
             onClick={openOptionsPage}>
             Open Settings
-          </button>
-          <button
-            className={cn(
-              'rounded px-4 py-2 font-medium shadow hover:scale-105',
-              isLight ? 'bg-green-600 text-white hover:bg-green-700' : 'bg-green-500 text-white hover:bg-green-600',
-            )}
-            onClick={injectContentScript}>
-            {t('injectButton')}
           </button>
           <ToggleButton onClick={exampleThemeStorage.toggle}>{t('toggleTheme')}</ToggleButton>
         </div>
